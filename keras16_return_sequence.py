b3ad9459fab73545a57c1b84fb68b07f2f8b9761
4nchez <58410037+4nchez@users.noreply.github.com>
2019-12-17 10:46:21 +0900

    Update keras16_return_sequence.py

--- keras16_return_sequence.py
+++ keras16_return_sequence.py
@@ -19,6 +19,7 @@ model.add(LSTM(51, activation='relu', input_shape=(3,1), return_sequences=True))
 model.add(LSTM(30, activation='relu', return_sequences=True))
 model.add(LSTM(30, activation='relu', return_sequences=True))
 model.add(LSTM(30, activation='relu', return_sequences=True))
+model.add(LSTM(30))
 model.add(Dense(20))
 model.add(Dense(1))
 
