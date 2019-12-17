dfa3c373ab0cb61d860cdb57e443da08b1ee0009
4nchez <58410037+4nchez@users.noreply.github.com>
2019-12-17 18:08:51 +0900

    Update keras21_cnn1.py

--- keras21_cnn1.py
+++ keras21_cnn1.py
@@ -5,7 +5,9 @@ kernel_size = (3,3)
 
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 model = Sequential()
-model.add(Conv2D(7, (2,2), input_shape=(28, 28, 1))) #padding='same'
+model.add(Conv2D(7, (2,2),
+                 #padding='same',
+                 input_shape=(28, 28, 1))) 
 # model.add(Conv2D(16,(2,2)))
 # model.add(MaxPooling2D(3,3))
 # model.add(Conv2D(8,(2,2)))
